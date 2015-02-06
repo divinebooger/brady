5cec89f3d39ead77f7eae2e37abda6df8ef39fbb
Brady Huynh <bradyh@berkeley.edu>
2015-02-06 12:55:17 -0800

    lab3

--- lab3/DoubleChain.java
+++ lab3/DoubleChain.java
@@ -48,8 +48,15 @@ public class DoubleChain {
 	/** Returns a string representation of the DoubleChain. 
 	  * This is an extra challenge problem. */
 	public String toString() {
-		/* your code here */		
-		return null;
+		String d = new String();
+		DNode p = getFront();
+		if(p!=null){
+			while(p!=null){
+				d = d + Double.toString(p.val);
+				p = p.next;
+			}
+		}	
+		return "<["+ d + "]>";
 	}
 
 	public static class DNode {
