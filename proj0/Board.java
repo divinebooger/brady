72a437d094bbda740be9035e7db7cf6ac0b1c133
Brady Huynh <bradyh@berkeley.edu>
2015-02-09 18:32:52 -0800

    dis proj yo

---
+++ proj0/Board.java
@@ -0,0 +1,297 @@
+public class Board{
+	private int n = 8;
+	private boolean drawEmpty;
+	private Piece[][] dahPieces = new Piece[n][n];
+	private Piece selectedPiece = null;
+	private int sX;
+	private int sY;
+	private int turn = 0;
+	private int hasMoved = 0;
+	private int firePieces = 12;
+	private int waterPieces = 12;
+
+	private Board(boolean shouldBeEmpty){
+		/* if shouldbeEmpty is true, initialize empty board
+		else initialize default board */
+		drawEmpty = shouldBeEmpty;
+	    for (int i = 0; i < n; i+=1) {
+        	for (int j = 0; j < n; j+=1) {
+        		if(j == 0 && (i % 2 == 0)){
+        			dahPieces[i][j] = new Piece(true,this,i,j,"pawn");
+        		}
+        		if(j == 1 && (i % 2 == 1)){
+        			dahPieces[i][j] = new Piece(true,this,i,j,"shield");
+        		}
+        		if(j == 2 && (i % 2 == 0)){
+        			dahPieces[i][j] = new Piece(true,this,i,j,"bomb");
+        		}
+        		if(j == (n-1) && (i % 2 == 1)){
+        			dahPieces[i][j] = new Piece(false,this,i,j,"pawn");
+        		}
+        		if(j == (n-2) && (i % 2 == 0)){
+        			dahPieces[i][j] = new Piece(false,this,i,j,"shield");
+        		}
+        		if(j == (n-3) && (i % 2 == 1)){
+        			dahPieces[i][j] = new Piece(false,this,i,j,"bomb");
+        		}
+
+			}
+		}
+	}
+
+	private void drawTheBoard(){
+		StdDrawPlus.setXscale(0,n);
+		StdDrawPlus.setYscale(0,n);
+        for (int i = 0; i < n; i+=1) {
+            for (int j = 0; j < n; j+=1) {
+                if ((i + j) % 2 == 0) StdDrawPlus.setPenColor(StdDrawPlus.GRAY);
+                else StdDrawPlus.setPenColor(StdDrawPlus.RED);
+                if(pieceAt(i,j)!=null){
+                	if(pieceAt(i,j)==selectedPiece) StdDrawPlus.setPenColor(StdDrawPlus.WHITE);
+                }                
+                StdDrawPlus.filledSquare(i + .5, j + .5, .5);
+			}
+		}
+		if(drawEmpty == false){
+			for(int i = 0; i < n; i+=1){
+				for(int j = 0; j<n; j+=1){
+					if(dahPieces[i][j]!=null){
+						StdDrawPlus.picture(i + .5, j + .5, Board.drawPieceLoc(dahPieces[i][j]),1,1);
+					}
+
+				}
+			}
+		}
+	}
+
+	private static String drawPieceLoc(Piece disPiece){
+		String king;
+		String type;
+		String team;
+		if(disPiece.isKing()) king = "-crowned";
+		else king = "";
+		if(disPiece.isBomb()) type = "bomb";
+		else if (disPiece.isShield()) type = "shield";
+		else type = "pawn";
+		if (disPiece.side() == 0) team = "fire";
+		else team = "water";
+		return "img/" + type + "-" + team +  king + ".png";				
+	}
+	public Piece pieceAt(int x, int y){
+		if(x >= n || y >= n){
+			return null;
+		}
+		return dahPieces[x][y];
+	}
+
+	
+
+	public boolean canSelect(int x, int y){
+		if(pieceAt(x,y)!=null){
+			if(pieceAt(x,y).side() == (turn%2) && (selectedPiece == null || pieceAt(x,y)!= null && hasMoved == 0)){
+				return true;
+			}
+		}
+		else if(selectedPiece!=null){
+			if(validMove(sX,sY,x,y) && (hasMoved == 0 || selectedPiece.hasCaptured())){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean redValidMove(int xi, int yi, int xf, int yf){
+		if(yf == yi + 1 && (xf == xi + 1 || xf == xi - 1)){
+			if(pieceAt(xf,yf)==null){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else if(yf == yi + 2 && xf == xi + 2){
+			if(pieceAt(xf - 1, yf - 1)!=null){
+				if(pieceAt(xf - 1, yf - 1).side() != (turn%2)){
+					return true;
+				}
+			}
+		}
+		else if(yf == yi + 2 && xf == xi - 2){
+			if(pieceAt(xf + 1, yf - 1)!=null){
+				if(pieceAt(xf + 1, yf - 1).side() != (turn%2)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean blueValidMove(int xi, int yi, int xf, int yf){
+		if(yf == yi - 1 && (xf == xi + 1 || xf == xi - 1)){
+			if(pieceAt(xf,yf)==null){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else if(yf == yi - 2 && xf == xi + 2){
+			if(pieceAt(xf - 1, yf + 1)!=null){
+				if(pieceAt(xf - 1, yf + 1).side() != (turn%2)){
+					return true;
+				}
+			}
+		}
+		else if(yf == yi - 2 && xf == xi - 2){
+			if(pieceAt(xf + 1, yf + 1)!=null){
+				if(pieceAt(xf + 1, yf + 1).side() != (turn%2)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean validMove(int xi, int yi, int xf, int yf){
+		System.out.println("xi=" +Integer.toString(xi));
+		System.out.println("yi=" +Integer.toString(yi));
+		if(pieceAt(xi,yi).isKing()){
+			return (redValidMove(xi, yi, xf, yf) || blueValidMove(xi, yi, xf, yf));
+		}
+		else if((turn%2)==0){
+			return redValidMove(xi, yi, xf, yf);
+		}
+		return blueValidMove(xi, yi, xf, yf);
+	}
+
+	public void select(int x, int y){
+		if(canSelect(x,y)){
+			selectedPiece = pieceAt(x,y);
+			sX = x;
+			sY = y;
+		}
+	}
+
+	/**private int xLocation(Piece p){
+	    for (int i = 0; i < n; i+=1) {
+        	for (int j = 0; j < n; j+=1) {
+        		if (dahPieces[i][j] == p){
+        			return i;
+        		}
+        	}
+		}
+		return n+1;
+	}
+
+	private int yLocation(Piece p){
+	    for (int i = 0; i < n; i+=1) {
+        	for (int j = 0; j < n; j+=1) {
+        		if (dahPieces[i][j] == p){
+        			return j;
+        		}
+        	}
+		}
+		return n+1;
+	}
+**/
+
+	public void place(Piece p, int x, int y){
+		if (x<n && y<n){
+			for (int i = 0; i < n; i+=1) {
+        		for (int j = 0; j < n; j+=1) {
+        			if (dahPieces[i][j] == p){
+        				dahPieces[i][j] = null;
+        			}
+        		}
+        	}
+	        dahPieces[x][y] = p;
+		}
+		hasMoved += 1;
+		sX = x;
+		sY = y;
+	}
+
+	public Piece remove(int x, int y){
+		Piece p;
+		if(x >= n && y >= n){
+			System.out.println("OUT OF BOUNDS");
+			return null;
+		}
+		else if(pieceAt(x,y)==null){
+			System.out.println("NO PIECE HERE BROTHER");
+			return null;
+		}
+		else{
+			p = pieceAt(x,y);
+			dahPieces[x][y] = null;
+			if(p.side() == 0){
+				firePieces-=1;
+			}
+			else if(p.side() == 1){
+				waterPieces-=1;
+			}
+			return p;
+		}
+	}
+
+	public boolean canEndTurn(){
+		if(selectedPiece != null){
+			if(selectedPiece.hasCaptured() == true || hasMoved > 0){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void endturn(){
+		if(canEndTurn()){
+			turn = turn + 1;
+			selectedPiece.doneCapturing();
+			selectedPiece = null;
+			hasMoved = 0;
+		}
+
+	}
+
+	public String winner(){
+		if(waterPieces == 0 && firePieces == 0){
+			return "No one";
+		}
+		else if(firePieces==0){
+			return "Water";
+		}
+		else if(waterPieces==0){
+			return "Fire";
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Board disBoard = new Board(false);
+		while(true){
+			disBoard.drawTheBoard();
+			if(StdDrawPlus.mousePressed()){
+				double xd = StdDrawPlus.mouseX();
+				int x = (int)xd;
+				double yd = StdDrawPlus.mouseY();
+				int y = (int)yd;
+				if (disBoard.canSelect(x,y)){
+					if(disBoard.pieceAt(x,y)!=null){
+						disBoard.select(x,y);
+					}
+					else{
+						if (disBoard.validMove(disBoard.sX,disBoard.sY,x,y)){
+							disBoard.selectedPiece.move(x,y);
+						}
+					}
+				}
+			}
+			if(StdDrawPlus.isSpacePressed()){
+				disBoard.endturn();
+			}
+		disBoard.winner();
+		StdDrawPlus.show(25);
+		}
+		
+	}
+}
---
+++ proj0/Piece.java
@@ -0,0 +1,165 @@
+public class Piece{
+	private boolean pieceTeam;
+	private Board currBoard;
+	private int xPosition;
+	private int yPosition;
+	private String typePiece;
+	private boolean kingMe = false;
+	private boolean didICap = false;
+	private int n = 8;
+	public Piece(boolean isFire, Board b, int x, int y, String type){
+		pieceTeam = isFire;
+		currBoard = b;
+		xPosition = x;
+		yPosition = y;
+		typePiece = type;
+	}
+
+	public boolean isFire(){
+		return pieceTeam;
+	}
+
+	public int side(){
+		if(pieceTeam){
+			return 0;
+		}
+		else{
+			return 1;
+		}
+
+	}
+
+	public boolean isKing(){
+		if(kingMe){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	private void theKinger(){
+		if(side()==0){
+			if(yPosition == (n-1)){
+				kingMe = true;
+			}
+		}
+		else if(side()==1){
+			if(yPosition == 0){
+				kingMe = true;
+			}
+		}
+	}
+
+	public boolean isBomb(){
+		if(typePiece == "bomb"){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public boolean isShield(){
+		if(typePiece == "shield"){
+			return true;
+		}
+		else{
+			return false;
+		}
+
+	}
+
+/**	private void redMove(int x, int y){
+		if(currBoard.validMove(xPosition,yPosition,x,y)){
+			if (x - xPosition > 1) {
+				currBoard.remove(x-1,y-1);
+				didICap = true;
+			}
+			else if(xPosition - x > 1){
+				currBoard.remove(x+1,y-1);
+				didICap = true;
+			}
+			xPosition = x;
+			yPosition = y;
+			currBoard.place(this,x,y);	
+		}		
+	}
+
+	private void blueMove(int x, int y){
+		if(currBoard.validMove(xPosition,yPosition,x,y)){
+			if (x - xPosition > 1) {
+				currBoard.remove(x-1,y+1);
+				didICap = true;
+			}
+			else if(xPosition - x > 1){
+				currBoard.remove(x+1,y+1);
+				didICap = true;
+			}
+			xPosition = x;
+			yPosition = y;
+			currBoard.place(this,x,y);	
+		}		
+	}
+
+	public void move(int x, int y){
+		if(side()==0){
+			redMove(x,y);
+		}
+		else if(side()==1){
+			blueMove(x,y);
+		}
+	}
+**/
+	public void move(int x, int y){
+			if (y > yPosition){
+				if (x - xPosition > 1) {
+					currBoard.remove(x-1,y-1);
+					didICap = true;
+				}
+				else if(xPosition - x > 1){
+					currBoard.remove(x+1,y-1);
+					didICap = true;
+				}
+			}
+			if(y < yPosition){
+				if (x - xPosition > 1) {
+					currBoard.remove(x-1,y+1);
+					didICap = true;
+				}
+				else if(xPosition - x > 1){
+					currBoard.remove(x+1,y+1);
+					didICap = true;
+				}
+			}
+			xPosition = x;
+			yPosition = y;
+			currBoard.place(this,x,y);
+			theKinger();
+			bomberMan();	
+		}
+	
+
+	private void bomberMan(){
+		if(isBomb() && hasCaptured()){
+	        for (int i = xPosition-1; (i <= xPosition + 1) && (i < n); i+=1) {
+	            for (int j = yPosition-1; (j <= yPosition + 1) && (j < n); j+=1) {
+	            	if(currBoard.pieceAt(i,j)!=null){
+	            		if(!(currBoard.pieceAt(i,j).isShield())){
+		            		currBoard.remove(i,j);
+		            	}
+	            	}
+	            }
+			}
+		}
+	}
+
+	public boolean hasCaptured(){
+		return didICap;
+
+	}
+
+	public void doneCapturing(){
+		didICap = false;
+	}
+}
